a66afd7f06730e4c8ca61b2b1268d188c9c1c306
Sangeeta244 <sangeeta07062004@gmail.com>
2025-06-23 13:46:25 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,8 @@
+var headerTitle = document.getElementById('header-title');
+
+headerTitle.textContent='Education ADDA';
+headerTitle.textContent='Education Masala';
+console.log(headerTitle);
+headerTitle.innerHTML='<h1>Sangeeta Singh </h1>';
+var header= document.getElementById('Sangeeta singh');
+header.style.borderBottom='10px solid #663399';
